2609bd59fe7096a5e43e235d3ede2cc19305a858
Sandeep Singh <sandeep.singh@exotel.in>
2019-02-07 09:53:31 +0530

    Initial commit

---
+++ types/types.go
@@ -0,0 +1,21 @@
+/*
+ **********************
+ * Sandeep Singh
+ **********************
+ */
+
+package types
+
+//Job structure
+//ID: job id
+//Priority: priority of job
+//TTR: time to run
+//Data: job byte data
+//Index: require for heap operations
+type Job struct {
+	ID       int
+	Priority int
+	TTR      int
+	Data     []*byte
+	Index    int
+}
