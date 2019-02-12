d41b6ad076658867b49ff0d61f68405235264bd1
Sandeep Singh <sandeep.singh@exotel.in>
2019-02-12 10:58:13 +0530

    V2.0 completed

--- exoqueue/exoqueue.go
+++ exoqueue/exoqueue.go
@@ -11,6 +11,8 @@ import (
 	"exotel/exobeanstalkd/tube"
 	"exotel/exobeanstalkd/types"
 	"fmt"
+	"sync"
+	"time"
 )
 
 type foo struct {
@@ -34,6 +36,7 @@ type Queue struct {
 	tubeNameToTubeID map[string]int
 	nextJobID        int
 	reserveTube      map[int]foo
+	mux              sync.Mutex
 }
 
 //New returns empty queue with default tube
@@ -113,12 +116,14 @@ func (q *Queue) push(tubeID int, job *types.Job) {
 }
 
 //Reserve reserves the job from watch list
-// and returns (jobID,data,error)
-func (q *Queue) Reserve() (int, []byte, error) {
+// and returns (chan jobID,chan data,chan error)
+func (q *Queue) Reserve(jobID chan int, data chan []byte, err chan error) { //(chan int, chan []byte,chan error) {
 	readyTubeID := -1
 	highPriority := 1 << 32
 	var priority int
 	var ok error
+
+	q.mux.Lock()
 	for tbID := range q.watchList {
 		priority, ok = (q.tubes[tbID]).Top()
 		if (ok == nil) && (highPriority > priority) {
@@ -126,25 +131,68 @@ func (q *Queue) Reserve() (int, []byte, error) {
 			readyTubeID = tbID
 		}
 	}
+	q.mux.Unlock()
+
 	if readyTubeID == -1 {
-		return 0, []byte{}, errors.New("No job in watchlist")
+		jobID <- 0
+		data <- []byte{}
+		err <- errors.New("No job in watchlist")
+		return
+		//return 0, []byte{}, errors.New("No job in watchlist")
 	}
+
+	q.mux.Lock()
 	job, _ := (q.tubes[readyTubeID]).Pop()
 	q.reserveTube[job.ID] = foo{readyTubeID, job}
 	delete(q.jobIDtoTubeID, job.ID)
-	//TO-DO support for TTR
-	delete(q.reserveTube, job.ID)
-	return job.ID, job.Data, nil
+	q.mux.Unlock()
+
+	jobID <- job.ID
+	data <- job.Data
+	err <- nil
+
+	//sleep for TTR seconds
+	time.Sleep(time.Duration(job.TTR) * time.Second)
+
+	q.mux.Lock()
+	defer q.mux.Unlock()
+	f, ok2 := q.reserveTube[job.ID]
+	if ok2 { //if job is running
+		delete(q.reserveTube, job.ID)
+		q.push(f.tubeID, job)
+	}
+	return
+}
+
+func (q *Queue) restoreJob(job *types.Job) {
+	//sleep for TTR seconds
+	time.Sleep(time.Duration(job.TTR) * time.Second)
+
+	q.mux.Lock()
+	f, ok := q.reserveTube[job.ID]
+	if ok { //if job is running
+		delete(q.reserveTube, job.ID)
+		q.push(f.tubeID, job)
+	}
+	q.mux.Unlock()
 }
 
 //Delete deletes the job from queue and returns id of deleted job
 //Does not support TTR
 func (q *Queue) Delete(jobID int) (int, error) {
+	q.mux.Lock()
+	defer q.mux.Unlock()
+
 	tubeID, ok := q.jobIDtoTubeID[jobID]
-	if !ok {
-		err := fmt.Sprintf("No job with ID %d", jobID)
-		return -1, errors.New(err)
+	if !ok { //if job is not in readytubes then it might be in reservetube
+		_, ok = q.reserveTube[jobID]
+		if !ok { //if job is not in reservetube
+			err := fmt.Sprintf("No job with ID %d", jobID)
+			return -1, errors.New(err)
+		}
+		delete(q.reserveTube, jobID)
+	} else {
+		_, _ = (q.tubes[tubeID]).Pop()
 	}
-	job, _ := (q.tubes[tubeID]).Pop()
-	return job.ID, nil
+	return jobID, nil
 }
